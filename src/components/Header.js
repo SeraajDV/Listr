4174c12961b8ba29f98e6fec9b56b93d4ff31193
SeraajDV <seraajdevilliers@gmail.com>
2020-09-21 15:07:35 +0200

    complete

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+import animationData from '../lottie/logo.json'
+ 
+export default function Header() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+    
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <h1 className="header">Listr</h1>
+        <Lottie 
+          options={defaultOptions}
+          height={50}
+          width={50}
+          style={{margin: 0}}
+        />
+      </div>
+    );
+}
